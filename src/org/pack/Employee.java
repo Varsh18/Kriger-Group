27403d139507f005cc1bdadbfb37de906ca8d806
varsh18 <varshiniravi1824@gmail.com>
2018-11-07 16:39:53 +0530

    Create connection in constructor

--- src/org/pack/Employee.java
+++ src/org/pack/Employee.java
@@ -4,5 +4,13 @@ public class Employee {
 	private  Connection con;
     private PreparedStatement ps;
     private ResultSet rs;
-    
+    public Employee(){
+    	try{
+        Class.forName("com.mysql.jdbc.Driver"); 
+        con=DriverManager.getConnection("jdbc:mysql://localhost/kriger","root","");
+    	}catch(ClassNotFoundException e) {
+    	System.out.println(e);
+    	}catch(Exception se){
+    		System.out.println(se);}
+    	   }
 }
