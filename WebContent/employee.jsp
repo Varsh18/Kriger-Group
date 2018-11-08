6fbfb9c618c18a23d9b5615162469ea575606cb1
varsh18 <varshiniravi1824@gmail.com>
2018-11-08 15:47:12 +0530

    Create employee personel in database

--- WebContent/employee.jsp
+++ WebContent/employee.jsp
@@ -9,11 +9,24 @@
 </head>
 <body style="Background-color:#CCC0FF">
 <%
-String name=request.getParameter("name");
-String lname=request.getParameter("lname");
-
+String e[]=new String[11];
+ e[0]=request.getParameter("name");
+ e[1]=request.getParameter("lname");
+ e[2]=request.getParameter("gender");
+ e[3]=request.getParameter("fname");
+ e[4]=request.getParameter("mname");
+ e[5]=request.getParameter("dob");
+ e[6]=request.getParameter("jdate");
+ e[7]=request.getParameter("role");
+ e[8]=request.getParameter("rhead");
+ e[9]=request.getParameter("bloodgroup");
+ e[10]=request.getParameter("type");
+ for(int i=0;i<e.length;i++)
+	 out.println(e[i]+"\n");
 Employee emp =new Employee();
-emp.insertEmployee(name,lname);
+emp.insertEmployee(e);
+out.println("Employee id:"+emp.count());
+out.println("Employee password:"+e[0].concat("@kriger123"));
 %>
 </body>
 </html>
--- src/org/pack/Employee.java
+++ src/org/pack/Employee.java
@@ -1,5 +1,7 @@
 package org.pack;
 import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 public class Employee {
 	private  Connection con;
     private PreparedStatement ps;
@@ -18,31 +20,29 @@ public class Employee {
     		System.out.println(se);}
     	   }
     //Insert data into the  table
-    public int insertEmployee(String fname, String lname){
+    public int insertEmployee(String[] emp){
     	int c=0;
+    	java.sql.Date dob=null,jdate=null;
+         java.util.Date		dateStr=null;
+    	SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+    	try {		
+    		dateStr= format.parse(emp[5]);
+	    	dob = new java.sql.Date(dateStr.getTime());
+	    	 dateStr = format.parse(emp[6]);
+	    	jdate = new java.sql.Date(dateStr.getTime());
+		} catch (ParseException e1) {
+			e1.printStackTrace();
+		} 
     	try {
 			c=count()+1;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-    	//sql="INSERT INTO kriger.employee_personel VALUES(?,?,?);";
     	try {
-			s.executeUpdate("INSERT INTO kriger.employee_personel(id,fname,lname) Values('"+c+"','"+fname+"','"+lname+"')");
+			s.executeUpdate("INSERT INTO kriger.employee_personel(id,password,name,lname,gender,fname,mname,dob,jdate,role,rhead,bloodgroup,type) Values('"+c+"','"+emp[0].concat("@kriger123")+"','"+emp[0]+"','"+emp[1]+"','"+emp[2]+"','"+emp[3]+"','"+emp[4]+"','"+dob+"','"+jdate+"','"+emp[7]+"','"+emp[8]+"','"+emp[9]+"','"+emp[10]+"')");
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	/*
-    	try{
-        ps=con.prepareStatement(sql);
-        
-        ps.setInt(1, c);
-        ps.setString(2, fname);
-        ps.setString(3, lname);
-    	ps.executeUpdate();
-    	}catch(SQLException se){
-    		System.out.println(se);
-    	}*/
     	return c;
     	 }
      public int count() throws Exception {
@@ -59,8 +59,20 @@ public class Employee {
     	}
         return 0;
     	}
-     /*public static void main(String[] args) {
+  /*   public static void main(String[] args) {
     	 Employee e=new Employee();
-    	 e.insertEmployee("h","y");
+    	 String[] emp=new String[11];
+    	 emp[0]="name";
+    	 emp[1]="lname";
+    	 emp[2]="gender";
+    	 emp[3]="fname";
+    	 emp[4]="mname";
+    	 emp[5]="24-07-2016";
+    	 emp[6]="12-09-1999";
+    	 emp[7]=("role");
+    	 emp[8]=("rhead");
+    	 emp[9]=("bloodgroup");
+    	 emp[10]=("type");
+    	 e.insertEmployee(emp);
      }*/
 }
